027671b9ff453965cbe962c7d9f2aad7df7c8916
mantas <mantas.petrauskas96@gmail.com>
2021-05-25 12:21:24 +0300

    EventDetails.vue: display event time string (happened x days ago/starts in x days)

--- components/EventDetails.vue
+++ components/EventDetails.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     <p>{{ event.stage }}</p>
+    <p class="text-sm">{{ eventTimeString }}</p>
   </div>
 </template>
 
 <script>
+import { formatDistanceToNow } from 'date-fns';
+
 export default {
   name: 'EventDetails',
 
@@ -12,6 +15,36 @@ export default {
     event: {
       type: Object,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      eventTimeString: '',
+      intervalId: null,
+    };
+  },
+
+  created() {
+    this.setEventTimeString();
+    this.intervalId = setInterval(this.setEventTimeString, 1000);
+  },
+
+  beforeDestroy() {
+    clearInterval(this.intervalId);
+  },
+
+  methods: {
+    setEventTimeString() {
+      let string = '';
+      const now = new Date();
+      const eventDate = new Date(`${this.event.date} ${this.event.time}`);
+      string += now > eventDate ? 'Happened ' : 'Starts in ';
+      string += formatDistanceToNow(eventDate);
+      if (now > eventDate) {
+        string += ' ago';
+      }
+      this.eventTimeString = string;
     },
   },
 };
